43a8b99608c4a8bc49765d7cda3f6eeacc6f40b5
Varnerlab <jdv27@cornell.edu>
2022-02-20 09:30:02 -0500

    Create test_ann_model.jl
    diff loss functions

---
+++ test_ann_model.jl
@@ -0,0 +1,33 @@
+include("Include.jl")
+
+# load the data -
+path_data_file = joinpath(_PATH_TO_DATA, "Thrombin-TF.csv")
+experimental_data_table = load(path_data_file)
+
+# get input and output data -
+input_data = convert.(Float32,Matrix(experimental_data_table[!,3:13]))
+output_data = convert.(Float32, Matrix(experimental_data_table[!,14:18]))
+training_data = zip(input_data, output_data)
+
+# build a model architecture -
+deep_coag_model = Chain(Dense(11, 8, σ), Dense(8, 5));
+
+# setup a loss function -
+loss(x, y) = Flux.Losses.logitcrossentropy(deep_coag_model(x), y)
+
+# pointer to params -
+ps = Flux.params(deep_coag_model)
+
+# use old school gradient descent -
+opt = Descent(0.1)
+
+# train -
+using Flux: @epochs
+@epochs 1000 Flux.train!(loss, ps, [(transpose(input_data), transpose(output_data))], opt)
+
+
+
+
+
+
+
