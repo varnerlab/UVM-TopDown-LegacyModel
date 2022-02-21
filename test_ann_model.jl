93a2826350e5d58527ceb469a07bce9ca9e40d49
Varnerlab <jdv27@cornell.edu>
2022-02-21 07:27:28 -0500

    hmmm
    only learning the mean

--- test_ann_model.jl
+++ test_ann_model.jl
@@ -7,7 +7,7 @@ experimental_data_table = load(path_data_file)
 # get input and output data -
 input_data = convert.(Float32,Matrix(experimental_data_table[!,3:13]))
 output_data = convert.(Float32, Matrix(experimental_data_table[!,14:18]))
-training_data = zip(input_data, output_data)
+training_data = [(transpose(input_data), transpose(output_data))]
 
 # build a model architecture -
 deep_coag_model = Chain(Dense(11, 7, σ), Dense(7, 5));
@@ -18,13 +18,24 @@ loss(x, y) = Flux.Losses.mae(deep_coag_model(x), y)
 # pointer to params -
 ps = Flux.params(deep_coag_model)
 
-# use old school gradient descent -
-opt = Descent(0.1)
+# # use old school gradient descent -
+opt = Momentum(0.05,0.95)
 
-# train -
+# # train -
 using Flux: @epochs
-@epochs 5000 Flux.train!(loss, ps, [(transpose(input_data), transpose(output_data))], opt)
+using BSON: @save
+@epochs 5000 Flux.train!(loss, ps, training_data, opt)
 
+# save -
+@save "deep_coag_model.bson" deep_coag_model
+
+Z = zeros(30,5)
+for i ∈ 1:30
+    tmp = deep_coag_model(input_data[i,:])
+    for j ∈ 1:5
+        Z[i,j] = tmp[j]
+    end
+end
 
 
 
