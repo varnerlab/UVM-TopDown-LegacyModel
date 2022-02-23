b77d25b37f3660f5e3d8ebf4d64425f34ba88f89
Varnerlab <jdv27@cornell.edu>
2022-02-23 09:29:31 -0500

    sample
    sampling

--- Include.jl
+++ Include.jl
@@ -18,6 +18,7 @@ using StatsBase
 using Statistics
 using NumericalIntegration
 using Distributions
+using PrettyTables
 using BSON: @load
 using BSON: @save
 
--- sample_ann_model.jl
+++ sample_ann_model.jl
@@ -8,8 +8,10 @@ path_data_file = joinpath(_PATH_TO_DATA, "Thrombin-TF.csv")
 experimental_data_table = load(path_data_file)
 
 # get input and output data -
+number_training_samples = 0
 input_data = convert.(Float32, Matrix(experimental_data_table[!, 3:13]))
 output_data = convert.(Float32, Matrix(experimental_data_table[!, 14:18]))
+test_output_data = convert.(Float32, Matrix(experimental_data_table[!, 14:18]))[(number_training_samples+1):end,:]
 
 # compute the cov matrix -
 Σ = cov(input_data)
@@ -49,4 +51,31 @@ for i ∈ 1:number_of_samples
     end
 end
 
+# sample the random -
 sample_output_array_random = sample(deep_coag_model, sample_input_array_random)
+μ_normal = mean(sample_output_array_normal,dims=1)
+σ_normal = std(sample_output_array_normal,dims=1)
+μ_random = mean(sample_output_array_random,dims=1)
+σ_random = std(sample_output_array_random,dims=1)
+μ_data = mean(test_output_data,dims=1)
+σ_data = std(test_output_data,dims=1)
+
+# create a pretty table -
+table_data_array = Array{Any,2}(undef, number_of_outputs, 7)
+label_data_array = ["τ_lag (min)", "peak (nM)", "τ_peak (min)", "max rate (nM/min)", "AUC (nM*min)"]
+header_data = (["quantity","μ_normal","σ_normal","μ_random","σ_random", "μ_data","σ_data"])
+
+for i ∈ 1:number_of_outputs
+    table_data_array[i,1] = label_data_array[i]
+    table_data_array[i,2] = μ_normal[i]
+    table_data_array[i,3] = σ_normal[i]
+    table_data_array[i,4] = μ_random[i]
+    table_data_array[i,5] = σ_random[i]
+    table_data_array[i,6] = μ_data[i]
+    table_data_array[i,7] = σ_data[i]
+end
+
+
+pretty_table(table_data_array; header=header_data)
+
+
